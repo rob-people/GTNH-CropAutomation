d1c12472d7f368d136a9d9c7d2ce44dce13c2e92
Rob People <robert.j.people@gmail.com>
2025-06-07 19:50:42 +1000

    Update reference

--- setup.lua
+++ setup.lua
@@ -26,10 +26,11 @@ end
 if #args >= 2 then
     repo = args[2]
 else
-    repo = 'https://raw.githubusercontent.com/DylanTaylor1/GTNH-CropAutomation/'
+    repo = 'https://raw.githubusercontent.com/DylanTaylor1/GTNH-CropAutomation/refs/heads/'
 end
 
 -- INSTALL
 for i=1, #scripts do
     shell.execute(string.format('wget -f %s%s/%s', repo, branch, scripts[i]))
 end
+
