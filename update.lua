3905f7e9b846786124b9f568aa87ccf8d691c24e
Rob People <robert.j.people@gmail.com>
2025-06-10 03:56:28 +1000

    Don't loop the reinstall

--- update.lua
+++ update.lua
@@ -17,13 +17,15 @@ local scripts = {
 -- UNINSTALL
 for i=1, #scripts do
     shell.execute(string.format('rm %s', scripts[i]))
-    shell.execute("rm config_default.lua")
-    shell.execute("mv config.lua config_temp.lua")
-    shell.execute("mv config.lua config_temp.lua")
-    print('Removed current installation, downloading latest version')
-    shell.execute(string.format('wget %s%s/setup.lua && setup', config.repoUrl, config.branch))
-    print('Restoring config')
-    shell.execute("mv config.lua config_default.lua")
-    shell.execute("mv config_temp.lua connfig.lua")
-    print(string.format('Updated to latest version of branch %s', config.branch))
+    print(string.format('Uninstalled %s', scripts[i]))
 end
+
+shell.execute("rm config_default.lua")
+shell.execute("mv config.lua config_temp.lua")
+shell.execute("mv config.lua config_temp.lua")
+print('Removed current installation, downloading latest version')
+shell.execute(string.format('wget %s%s/setup.lua && setup', config.repoUrl, config.branch))
+print('Restoring config')
+shell.execute("mv config.lua config_default.lua")
+shell.execute("mv config_temp.lua connfig.lua")
+print(string.format('Updated to latest version of branch %s', config.branch))
